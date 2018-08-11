8be03dacba04993ad504d2e14d01f606cab18517
github username <frank@thefrankharvey.com>
2018-08-11 11:57:20 -0400

    video player autoplays video from google drive, must store google drive links in redux store, now will make function to play random clips back to back and pause before quote.

--- frontend/src/components/VideoPlayer.js
+++ frontend/src/components/VideoPlayer.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react'
 
+
 class VideoPlayer extends Component {
+  
   render() {
     return (
       <div className="Player">
         <video controls
-       muted
-       src="/media/examples/stream_of_water.webm"
+       autoplay="autoplay"
+       src='https://drive.google.com/uc?id=1PpV-1H3CSrs3ewHuOkEvIQLIWVHctWhH'
        width="300"
        height="200">
-      Sorry, your browser doesn{"'"}t support embedded videos.
+       Sorry, your browser doesn{"'"}t support embedded videos.
         </video>
       </div>
     );
