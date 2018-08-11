e12039c2a75a9da823ed4205fda29bfacb60e8c3
github username <frank@thefrankharvey.com>
2018-08-11 10:50:30 -0400

    deleted default React App stuff

--- frontend/src/VideoPlayer.js
+++ frontend/src/VideoPlayer.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
+
 class VideoPlayer extends Component {
   render() {
     return (
-      <div className="App">
-        <p> hey I play videos</p>
+      <div className="Player">
+        <video controls
+       muted
+       src="/media/examples/stream_of_water.webm"
+       width="300"
+       height="200">
+      Sorry, your browser doesn't support embedded videos.
+        </video>
       </div>
     );
   }
