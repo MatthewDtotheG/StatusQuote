259af999bcac55f8cb17de2196fa7bddee17b873
github username <frank@thefrankharvey.com>
2018-08-11 10:15:34 -0400

    added VideoPlayer component and imported to App.js

---
+++ frontend/src/VideoPlayer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class VideoPlayer extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p> hey I play videos</p>
+      </div>
+    );
+  }
+}
+
+export default VideoPlayer;
