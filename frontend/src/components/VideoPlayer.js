a677cbe762b226febc5da3ed7aaea5811a74a62d
github username <frank@thefrankharvey.com>
2018-08-11 17:47:52 -0400

    added hint

--- frontend/src/components/VideoPlayer.js
+++ frontend/src/components/VideoPlayer.js
@@ -9,30 +9,35 @@ class VideoPlayer extends Component {
       title: 'A League of Their Own',
       link: 'https://drive.google.com/uc?id=1PpV-1H3CSrs3ewHuOkEvIQLIWVHctWhH',
       quote: 'There\'s no crying in baseball',
+      hint: 'baseball',
       time: 10
     },
     {
       title: 'Back to the Future 2',
       link: 'https://drive.google.com/uc?id=1cE5TpS70IzZAbaM4T6mD7nUVrSW5pkOM',
       quote: 'Where we\'re going we don\'t need roads',
+      hint: 'roads',
       time: 9
     },
     {
       title: 'A Few Good Men',
       link: 'https://drive.google.com/uc?id=11_00hlcxRkB_wcgfvJ1rN50noaHN7YIN',
       quote: 'You can\'t handle the truth',
+      hint: 'truth',
       time: 5
     },
     {
       title: 'Star Wars: The Empire Strikes Back',
       link: 'https://drive.google.com/uc?id=1QEBSxyChMLigatKLYerrfj4b-6jFxCoI',
       quote: 'Try not Do or do not There is no try',
+      hint: 'Try',
       time: 8
     }
   ],
     index: 0,
     pause: false,
-    firstPause: false
+    firstPause: false,
+    hintClicked: false
 }
 
   next = () => {
@@ -63,6 +68,15 @@ class VideoPlayer extends Component {
     }
   }
 
+  hintyClicked = (time) => {
+    this.setState({
+      hintClicked: !this.state.hintClicked
+    })
+
+    console.log('time match', this.state.clips.find((obj) => obj.time == time))
+  }
+
+
   render() {
     return (
       <div className="Player">
@@ -74,6 +88,8 @@ class VideoPlayer extends Component {
        Sorry, your browser doesn{"'"}t support embedded videos.
         </video>
         {this.getPopup()}
+        <button onClick={() => this.hintyClicked(this.state.clips[this.state.index].time)} className='hint-btn'>Hint</button>
+        {this.state.hintClicked ? <h1></h1> : console.log('this aint')}
       </div>
     );
   }
