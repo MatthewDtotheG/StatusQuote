6d628f68df765bb4f50924590cee45ea602e6bb0
Helen Liutongco <liutongcoh@gmail.com>
2018-08-21 18:11:48 -0400

    Add start of end screen changes

--- frontend/src/components/EndGame.js
+++ frontend/src/components/EndGame.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import cageNic from '../cageNic.png'
-class EndGame extends React.Component {
-  render() {
-    return (
-      <div id="player-selection">
-        <div id="end-game">
-        <h1>Game Over</h1>
-        </div>
-        <iframe src="https://giphy.com/embed/3ohhwMfw0rU0LQfFeg" width="480" height="480" frameBorder="0" allowFullScreen></iframe>
-        <iframe src="https://giphy.com/embed/MUlt1JtQrTZXSfeZYN" width="480" height="480" frameBorder="0" allowFullScreen></iframe>
+import {connect} from 'react-redux'
+
+const EndGame = ({player1, player2}) => {
+  console.log(player1, player2, player1 === player2 && "It's a tie!" || "truthy value" );
+  return (
+    <div id="player-selection">
+      <div id="end-game">
+      <h1>{player1 === player2 && "It's a tie!" || player1 > player2 && "Player One Wins!" || "Player Two Wins!" }</h1>
       </div>
-    )
-  }
+      <iframe src="https://giphy.com/embed/3ohhwMfw0rU0LQfFeg" width="480" height="480" frameBorder="0" allowFullScreen></iframe>
+      <iframe src="https://giphy.com/embed/MUlt1JtQrTZXSfeZYN" width="480" height="480" frameBorder="0" allowFullScreen></iframe>
+    </div>
+  )
 }
 
-export default EndGame;
+const mapStateToProps = (store) => {
+  player1: store.player1
+  player2: store.player2
+}
+
+export default connect(mapStateToProps)(EndGame);
