d1f93cc437c847cc94514045c5c76b0842805c63
Helen Liutongco <liutongcoh@gmail.com>
2018-08-28 18:05:53 -0400

    Add winner text to end game screen

--- frontend/src/components/EndGame.js
+++ frontend/src/components/EndGame.js
@@ -2,12 +2,11 @@ import React from 'react'
 import cageNic from '../cageNic.png'
 import {connect} from 'react-redux'
 
-const EndGame = ({player1, player2}) => {
-  console.log(player1, player2, player1 === player2 && "It's a tie!" || "truthy value" );
+const EndGame = (props) => {
   return (
     <div id="player-selection">
       <div id="end-game">
-      <h1>{player1 === player2 && "It's a tie!" || player1 > player2 && "Player One Wins!" || "Player Two Wins!" }</h1>
+      <h1>{props.player1 === props.player2 && "It's a tie!" || props.player1 > props.player2 && "Player One Wins!" || "Player Two Wins!" }</h1>
       </div>
       <iframe src="https://giphy.com/embed/3ohhwMfw0rU0LQfFeg" width="480" height="480" frameBorder="0" allowFullScreen></iframe>
       <iframe src="https://giphy.com/embed/MUlt1JtQrTZXSfeZYN" width="480" height="480" frameBorder="0" allowFullScreen></iframe>
@@ -16,8 +15,10 @@ const EndGame = ({player1, player2}) => {
 }
 
 const mapStateToProps = (store) => {
-  player1: store.player1
-  player2: store.player2
+  return {
+    player1: store.player1,
+    player2: store.player2
+  }
 }
 
 export default connect(mapStateToProps)(EndGame);
